2f940398eb70f514ed9a0ba10ad447736ec8674e
Sujeesh <sujeeshdl@gmail.com>
2020-05-11 12:41:11 +0100

    Port change

--- src/app/dashboard/services/car.service.ts
+++ src/app/dashboard/services/car.service.ts
@@ -9,21 +9,21 @@ export class CarService {
     constructor(private http: HttpClient) { }
 
     getCarsSmall() {
-        return this.http.get<any>('http://localhost:4200/assets/cars.small.json')
+        return this.http.get<any>('http://localhost:3000/assets/cars.small.json')
             .toPromise()
             .then(res => res.data as Car[])
             .then(data => data);
     }
 
     getCarsMedium() {
-        return this.http.get<any>('http://localhost:4200/assets/cars.medium.json')
+        return this.http.get<any>('http://localhost:3000/assets/cars.medium.json')
             .toPromise()
             .then(res => res.data as Car[])
             .then(data => data);
     }
 
     getCarsLarge(first, rows, count) {
-        return this.http.get<any>('http://localhost:4200/assets/cars.large.json')
+        return this.http.get<any>('http://localhost:3000/assets/cars.large.json')
             .toPromise()
             .then(res => res.data as Car[])
             .then((data) => {
@@ -37,7 +37,7 @@ export class CarService {
     }
 
     getBulkData(first, rows, count) {
-        return this.http.get<any>('http://localhost:4200/assets/huge.data.json')
+        return this.http.get<any>('http://localhost:3000/assets/huge.data.json')
             .toPromise()
             .then((res) => {
                 return res as any[];
